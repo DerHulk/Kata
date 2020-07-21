a3903b9abd016b29c5ca22126413df87e1b0cada
thegrayhulk <thegrayhulk@googlemail.com>
2020-07-21 15:22:45 +0200

    Add some App-Code.

--- RussianPeasantMultiplication/dotnet/RPM.App/Program.cs
+++ RussianPeasantMultiplication/dotnet/RPM.App/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RPM.Core;
+using System;
 
 namespace RPM.App
 {
@@ -6,7 +7,27 @@ namespace RPM.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine($"Lets multiplicate some values...");
+
+            var calculator = new Calculator();
+            var x = GetValidUserInput("X");
+            var y = GetValidUserInput("Y");
+
+            var result = calculator.Mul(x, y);
+            System.Console.WriteLine($"Result is {result}");
+            System.Console.ReadLine();
+            System.Console.WriteLine($"Hit any key to exit.");
+        }
+
+        static int GetValidUserInput(string askFor)
+        {
+            var number = int.MaxValue;
+            do
+            {
+                System.Console.WriteLine($"please enter {askFor}");
+            } while (!int.TryParse(System.Console.ReadLine(), out number));
+
+            return number;
         }
     }
 }
